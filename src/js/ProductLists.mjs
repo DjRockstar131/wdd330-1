2b0bf3c8f785b086cf228b6d3431376b13908dbe
Davin Quist <davinjquist@gmail.com>
2026-01-17 16:32:53 -0700

    main

---
+++ src/js/ProductLists.mjs
@@ -0,0 +1,33 @@
+// src/js/ProductList.mjs
+import { renderListWithTemplate } from "./utils.mjs";
+
+function productCardTemplate(product) {
+  // Adjust property names here if your JSON uses different keys!
+  return `
+    <li class="product-card">
+      <a href="/product/index.html?product=${product.Id}">
+        <img src="${product.Images?.PrimaryLarge || product.Image}" alt="${product.Name}" />
+        <h2 class="card__brand">${product.Brand?.Name || product.Brand || ""}</h2>
+        <h3 class="card__name">${product.Name}</h3>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+      </a>
+    </li>
+  `;
+}
+
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+
+  async init() {
+    const list = await this.dataSource.getData(this.category);
+    this.renderList(list);
+  }
+
+  renderList(list) {
+    renderListWithTemplate(productCardTemplate, this.listElement, list, "afterbegin", true);
+  }
+}
