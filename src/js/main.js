2d66c292532bdad898694498c7cb61c35047b60f
Davin Quist <davinjquist@gmail.com>
2026-01-17 16:40:18 -0700

    main

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 
 import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
+import ProductList from "./ProductLists.mjs";
 
 const dataSource = new ProductData("tents");
 const listElement = document.querySelector(".product-list"); 
