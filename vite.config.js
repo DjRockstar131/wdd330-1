c200f5501fb47e07c5dd226bd5f066d4b3e7a248
Davin Quist <davinjquist@gmail.com>
2026-01-22 22:08:51 -0700

    main

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,13 @@ import { resolve } from "path";
 export default {
   root: resolve(__dirname, "src/public"),
   build: {
-    outDir: resolve(__dirname, "dist"),
-    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/public/index.html"),
+        products: resolve(__dirname, "src/public/product_pages/index.html"),
+        product: resolve(__dirname, "src/public/product_pages/product.html"), // ✅ ADD THIS
         cart: resolve(__dirname, "src/public/cart/index.html"),
         checkout: resolve(__dirname, "src/public/checkout/index.html"),
-        product_pages: resolve(__dirname, "src/public/product_pages/index.html"),
       },
     },
   },
