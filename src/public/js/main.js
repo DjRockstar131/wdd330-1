82969d2bcbae1832bad6bf6737802c1063850a1f
Davin Quist <davinjquist@gmail.com>
2026-01-22 21:37:31 -0700

    main

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,14 +1,3 @@
-
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
-
-const dataSource = new ProductData();
-const listElement = document.querySelector(".product-list");
-console.log("listElement:", listElement);
-const productList = new ProductList("tents", dataSource, listElement);
-productList.init();
-
-
