6012a2cf651cd7d4ea0764ba2b0358368ae2b1e6
Davin Quist <davinjquist@gmail.com>
2026-01-29 19:10:24 -0700

    main

---
+++ src/public/js/product_pages.js
@@ -0,0 +1,24 @@
+// src/public/js/product_pages.js
+
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+import ExternalServices from "/js/ExternalServices.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+
+loadHeaderFooter();
+
+async function init() {
+  // get product id from URL: ?product=XXXX
+  const productId = getParam("product");
+
+  if (!productId) {
+    console.error("Missing product id in URL");
+    return;
+  }
+
+  const dataSource = new ExternalServices();
+  const product = new ProductDetails(productId, dataSource);
+
+  await product.init();
+}
+
+init();
